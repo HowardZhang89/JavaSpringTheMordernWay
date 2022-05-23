ef2070c57b07105594b0639998e4889991b69fe4
Howard Zhang <howard.zhang89@gmail.com>
2022-05-22 20:15:58 -0500

    Section 3 done: Added Components and Autowiring
    Added Components and Autowiring

--- spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/BinarySearchImpl.java
@@ -1,17 +1,16 @@
 package com.in28minutes.com.basics.springin5steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySearchImpl {
 	
+	@Autowired
 	private SortAlgorithm sortAlgorithm;
-	
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-	}
-	
+
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		
-		SortAlgorithm sortAlgorithm = new BubbleSortAlgorithm();
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm);
 		
--- spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/BubbleSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.in28minutes.com.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		// Logic for Bubble Sort
