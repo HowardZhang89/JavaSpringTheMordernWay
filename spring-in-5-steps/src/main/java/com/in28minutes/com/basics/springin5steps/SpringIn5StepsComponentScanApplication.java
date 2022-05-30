c23219ce1a58fa9e42a7049b5d3793e2b23c04c4
Howard Zhang <howard.zhang89@gmail.com>
2022-05-30 16:31:40 -0500

    Refactor Scope, ComponentScan, CDI Application to also not use Spring Boot

--- spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/SpringIn5StepsComponentScanApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/SpringIn5StepsComponentScanApplication.java
@@ -2,15 +2,14 @@ package com.in28minutes.com.basics.springin5steps;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.com.basics.componentscan.ComponentDAO;
 
 
-@SpringBootApplication
+@Configuration
 @ComponentScan("com.in28minutes.com.basics.componentscan")
 public class SpringIn5StepsComponentScanApplication {
 
@@ -21,16 +20,16 @@ public class SpringIn5StepsComponentScanApplication {
 	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsComponentScanApplication.class);
 			
 	public static void main(String[] args) {
-		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 		
-		ApplicationContext applicationContext =
-				SpringApplication.run(SpringIn5StepsComponentScanApplication.class, args);
+		try(AnnotationConfigApplicationContext applicationContext =
+				new AnnotationConfigApplicationContext(SpringIn5StepsComponentScanApplication.class)){
 		
 		ComponentDAO componentDao =
 				applicationContext.getBean(ComponentDAO.class);
 
 		LOGGER.info("{}", componentDao);
 		
+		}	
 	}
 
 }
--- spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/SpringIn5StepsScopeApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/com/basics/springin5steps/SpringIn5StepsScopeApplication.java
@@ -2,13 +2,14 @@ package com.in28minutes.com.basics.springin5steps;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.com.basics.springin5steps.scope.PersonDAO;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SpringIn5StepsScopeApplication {
 
 	// What are the beans?
@@ -20,8 +21,8 @@ public class SpringIn5StepsScopeApplication {
 	public static void main(String[] args) {
 		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 		
-		ApplicationContext applicationContext =
-				SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
+		try(AnnotationConfigApplicationContext applicationContext =
+				new AnnotationConfigApplicationContext(SpringIn5StepsScopeApplication.class)){
 		
 		PersonDAO personDao =
 				applicationContext.getBean(PersonDAO.class);
@@ -35,6 +36,8 @@ public class SpringIn5StepsScopeApplication {
 		
 		LOGGER.info("{}", personDao2);
 		LOGGER.info("{}", personDao2.getJdbcConnection());
+		
+		}
 	}
-
+	
 }
