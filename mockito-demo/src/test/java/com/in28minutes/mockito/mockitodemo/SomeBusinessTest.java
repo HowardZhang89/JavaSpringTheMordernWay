a5d4248babdef88f44f153399f7c138eab1a4d67
Howard Zhang <howard.zhang89@gmail.com>
2022-06-05 01:38:38 -0500

    Section 6: Using a stub - disadvantages

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessTest.java
@@ -0,0 +1,25 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeBusinessTest {
+
+	@Test
+	public void testFindTheGreatestFromAllData() {
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
+		assertEquals(24, businessImpl.findTheGreatestFromAllData());
+	}
+
+}
+
+class DataServiceStub implements DataService {
+
+	@Override
+	public int[] retrieveAllData() {
+		// TODO Auto-generated method stub
+		return new int[]{24,6,15};
+	}
+	
+}
