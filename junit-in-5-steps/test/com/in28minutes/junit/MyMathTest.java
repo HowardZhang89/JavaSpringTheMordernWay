d6e92e9a2911cf87e6b99643f0046aa7184ca5d6
Howard Zhang <howard.zhang89@gmail.com>
2022-06-04 19:31:07 -0500

    Section 5 Step 3: First Code and First Unit Test
    Added MyMath.java and MyMathTest.java.

--- junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -5,10 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyMathTest {
-
+	
+	MyMath myMath = new MyMath(); 
+	//MyMath.sum
+	// 1,2,3 => 6
 	@Test
-	public void test() {
-		// absence of failure is success
+	public void sum_with3numbers() {
+		assertEquals(6, myMath.sum(new int[] {1,2,3}));
 	}
-
+	
+	@Test
+	public void sum_with1number() {
+		assertEquals(3, myMath.sum(new int[] {3}));
+	}
 }
