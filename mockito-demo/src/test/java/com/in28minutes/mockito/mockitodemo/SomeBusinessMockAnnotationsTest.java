166e97502f9578e9c5599c008d0dd7d573f3281e
Howard Zhang <howard.zhang89@gmail.com>
2022-06-05 13:48:32 -0500

    Section 6 Step 4: Using Mockito Annotations - @Mock, @InjectMocks, @RunWith

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockAnnotationsTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockAnnotationsTest.java
@@ -5,22 +5,12 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
-public class SomeBusinessMockTest {
-	
-	@Mock
-	DataService dataServiceMock;
-	
-	@InjectMocks
-	SomeBusinessImpl businessImpl;
-	
+public class SomeBusinessMockAnnotationsTest {
+
 	@Test
 	public void testFindTheGreatestFromAllData() {
+		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {24,15,3});
 		
 		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
@@ -29,18 +19,11 @@ public class SomeBusinessMockTest {
 	
 	@Test
 	public void testFindTheGreatestFromAllData_ForOneValue() {
+		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {15});
 		
 		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
 		assertEquals(15, businessImpl.findTheGreatestFromAllData());
-	}
-	
-	@Test
-	public void testFindTheGreatestFromAllData_ForNoValue() {
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
-		
-		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
-		assertEquals(Integer.MIN_VALUE, businessImpl.findTheGreatestFromAllData());
 	}
 
 	
