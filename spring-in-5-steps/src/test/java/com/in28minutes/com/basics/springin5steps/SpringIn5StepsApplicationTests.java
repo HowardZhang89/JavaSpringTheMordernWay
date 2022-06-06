d7949e602ac783f0dda0c555212c6cb2fa2ba48a
Howard Zhang <howard.zhang89@gmail.com>
2022-06-05 21:16:32 -0500

    Section 7: Spring Unit Test with Java Context

--- spring-in-5-steps/src/test/java/com/in28minutes/com/basics/springin5steps/SpringIn5StepsApplicationTests.java
+++ spring-in-5-steps/src/test/java/com/in28minutes/com/basics/springin5steps/SpringIn5StepsApplicationTests.java
@@ -1,9 +1,11 @@
 package com.in28minutes.com.basics.springin5steps;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
-@SpringBootTest
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
 class SpringIn5StepsApplicationTests {
 
 	@Test
---
+++ spring-in-5-steps/src/test/java/com/in28minutes/com/basics/springin5steps/basic/BinarySearchTest.java
@@ -0,0 +1,29 @@
+package com.in28minutes.com.basics.springin5steps.basic;
+
+import static org.junit.Assert.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.in28minutes.com.basics.springin5steps.SpringIn5StepsBasicApplication;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+//Load the context
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=SpringIn5StepsBasicApplication.class)
+public class BinarySearchTest {
+	
+	//Get the bean from the context
+	@Autowired
+	BinarySearchImpl binarySearch;
+	
+	@Test
+	public void testBasicScenario() {
+		// result is hard-coded so input doesn't matter
+		int result = binarySearch.binarySearch(new int[] {1, 2,3}, 5);
+		assertEquals(3, result);
+	}
+
+}
